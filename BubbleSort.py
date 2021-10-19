21655a4edc9859e756fdaee1a38d861d27e5c43b
Carlos Arango <47753930+carlosarango97@users.noreply.github.com>
2021-10-18 21:18:58 -0500

    Bubble sort file was added

---
+++ BubbleSort.py
@@ -0,0 +1,10 @@
+def bubble_sort(lista):
+    #implement code
+
+lista_ejemplo = [58, 52, 5, 12, 69, 87, 12]
+
+bubbleSort(lista_ejemplo)
+
+print ("Lista ordenada:")
+for i in lista_ejemplo:
+	print (str(i))
